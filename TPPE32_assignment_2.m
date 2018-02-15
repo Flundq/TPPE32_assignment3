3d2ec10bf08f6ec6efa10494f203827d26cc479b
Flundq <31475360+Flundq@users.noreply.github.com>
2018-02-15 14:13:49 +0100

    Add files via upload

--- TPPE32_assignment_2.m
+++ TPPE32_assignment_2.m
@@ -29,6 +29,7 @@ for i=1:length(performance)
 end
 
 semilogy(dates, perf);
+title('Performance of Equal Weighted Portfolio');
 datetick('x','YYYY-mm')
 
 %% Task 1
@@ -80,13 +81,15 @@ fun=@(x0)-sum((-log(EWMA_serie(x0, avg_ret)')-avg_ret(2:end).^2./EWMA_serie(x0, 
 
 EWMA = EWMA_serie(lambda,avg_ret)';
 
-VaR_ewma95 = norminv(0.95) * sqrt(EWMA(501:end));
-VaR_ewma99 = norminv(0.99) * sqrt(EWMA(501:end));
+VaR_ewma95 = 10*norminv(0.95) * sqrt(EWMA(501:end));
+VaR_ewma99 = 10*norminv(0.99) * sqrt(EWMA(501:end));
 
 figure()
 plot(dates(503:end), VaR_ewma95);
 hold on
 plot(dates(503:end), VaR_ewma99);
+title('VaR-estimates using EWMA, optimal lambda')
+legend('95-VaR','99-VaR')
 datetick('x','YYYY-mm');
 
 % 1c
@@ -102,17 +105,19 @@ bar(sorted_VpLoss)
 VaR95_his = zeros(length(avg_ret)-500,1);
 VaR99_his = VaR95_his;
 for i=1:length(VaR95_his)
-    VaR95_his(i) = -quantile(avg_ret(i:i+499)*10*(1),0.05);
-    VaR99_his(i) = -quantile(avg_ret(i:i+499)*10*(1),0.01);
+    VaR95_his(i) = -quantile(avg_ret(i:i+499)*10,0.05);
+    VaR99_his(i) = -quantile(avg_ret(i:i+499)*10,0.01);
 end
 
 figure()
 subplot(2,1,1)
 plot(dates(502:end), VaR95_his);
+title('His. Sim. VaR 95%');
 datetick('x','YYYY-mm')
 hold on
 subplot(2,1,2)
 plot(dates(502:end), VaR99_his)
+title('His. Sim. VaR 99%');
 datetick('x','YYYY-mm')
 
 % ES
@@ -121,11 +126,42 @@ E_fall99 = mean(sorted_VpLoss(1:floor((length(Vp_R)*0.01))));
 
 % 1d
 sigma_t = sqrt(sum((avg_ret(1:end)-mean(avg_ret(1:end))).^2)/(length(avg_ret)-1));
-for i=1:length(avg_ret)-1
+for i=1:length(EWMA)
     avg_retStd=avg_ret(2:end)*(sigma_t/sqrt(EWMA(i)));
 end
 figure();
 hist(avg_retStd,100);
 
+% VaR
+VaR95_hisStd = zeros(length(avg_retStd)-500,1);
+VaR99_hisStd = VaR95_hisStd;
+for i=1:length(VaR95_hisStd)
+    VaR95_hisStd(i) = -quantile(avg_retStd(i:i+499)*10,0.05);
+    VaR99_hisStd(i) = -quantile(avg_retStd(i:i+499)*10,0.01);
+end
 
+figure()
+subplot(2,1,1)
+plot(dates(503:end), VaR95_hisStd);
+title('His. Sim. VaR 95% STD return');
+datetick('x','YYYY-mm')
+hold on
+subplot(2,1,2)
+plot(dates(503:end), VaR99_hisStd)
+title('His. Sim. VaR 99% STD return');
+datetick('x','YYYY-mm')
+
+% 1e
+% FOR 1B
+%XT=length(find(avg_ret(502:end)<-VaR_ewma95));
+%m_=norminv(0.975,length(VaR_ewma95)*0.05,sqrt(length(VaR_ewma95)*0.05*0.95));
+% EWMA
+[ XT,m_ ] = testHypNor(0.01, 0.95, 2, 502, avg_ret, VaR_ewma95);
+[ XT,m_ ] = testHypNor(0.01, 0.99, 2, 502, avg_ret, VaR_ewma99);
+% Historical Simulation
+[ XT,m_ ] = testHypNor(0.01, 0.95, 2, 501, avg_ret, VaR95_his);
+[ XT,m_ ] = testHypNor(0.01, 0.99, 2, 501, avg_ret, VaR99_his);
+% Historical Simulation
+[ XT,m_ ] = testHypNor(0.01, 0.95, 2, 502, avg_ret, VaR95_hisStd);
+[ XT,m_ ] = testHypNor(0.01, 0.99, 2, 502, avg_ret, VaR99_hisStd);
 
